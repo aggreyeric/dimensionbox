90c4f1d7ad66bf23b76058ca2b0b1c88056f7da0
aggreyeric <ericaggrey@outlook.com>
2024-06-18 12:37:25 +0200

    Completed load all tables

--- src/jobs/initial_load/load_all_tables.py
+++ src/jobs/initial_load/load_all_tables.py
@@ -1,8 +1,5 @@
 from delta import *
-import importlib
 import os
-
-
 
 
 import re
@@ -26,20 +23,20 @@ import pyspark
 
 builder = pyspark.sql.SparkSession.builder.appName("loadData") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog").config("spark.ui.port", "8080")
 
 spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
 # logger =  logging.Log4j(spark)
 
-BASE_PATH = r"C:\Users\eric\Desktop\apps_\spakapps\dimensionbox\src\data\raw"
+BASE_PATH = r"C:\Users\Jengo\Desktop\notes_md\Data_Engineering\projectsproto\end_end_projects\dimensionbox\src\data\raw"
 list_paths = os.listdir(BASE_PATH)
 
 for path_ in list_paths:
     file_info = getext(path_)
     try:
         data = spark.read.format(file_info[1]).option("header", "true").load(os.path.join(BASE_PATH, path_))
-        data.write.format("parquet").saveAsTable(file_info[0])
+        data.write.format("delta").mode("overwrite").saveAsTable(file_info[0])
         # logger.info(f"Table {file_name} was loaded succesfully")
         
     except:
