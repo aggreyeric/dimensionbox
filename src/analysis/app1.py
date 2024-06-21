3f0e2d63856ac4bada5003a278b54a40531d9502
aggreyeric <ericaggrey@outlook.com>
2024-06-21 09:14:17 +0200

    Added apps

---
+++ src/analysis/app1.py
@@ -0,0 +1,8 @@
+from reuse import getspark, LAKE_URL
+from os import path
+
+spark = getspark() 
+
+df = spark.read.format("delta").option("header", True).load(path.join(LAKE_URL, "dim_address"))
+
+df.show()
